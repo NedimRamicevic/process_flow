6bdd6bf2138ceed340399e6807939764c0d27275
Nedim <mr.ramicevic@gmail.com>
2022-03-08 12:47:02 +0300

    providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:process_flow/provider/task_provider.dart';
-import 'package:process_flow/screens/tasks/task.dart';
-import 'package:process_flow/screens/tasks/taskList.dart';
-
-import 'screens/tasks/settingsForm.dart';
+import 'package:process_flow/screens/tasks/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,45 +12,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    void _showSettingsPanel() {
-      showModalBottomSheet(
-          context: context,
-          builder: (context) {
-            return Container(
-              padding: const EdgeInsets.all(10),
-              child: const SettingsForm(),
-            );
-          });
-    }
-
-    return MaterialApp(
-        title: 'Flutter Demo',
-        home: Scaffold(
-          body: Container(
-            color: Colors.amber,
-            child: const TaskList(),
-          ),
-          bottomNavigationBar: BottomAppBar(
-              shape: const CircularNotchedRectangle(),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: const [
-                  IconButton(
-                    icon: Icon(Icons.home),
-                    onPressed: null,
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.school),
-                    onPressed: null,
-                  ),
-                ],
-              )),
-          floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add),
-            onPressed: null,
-          ),
-          floatingActionButtonLocation:
-              FloatingActionButtonLocation.centerDocked,
-        ));
+    return const MaterialApp(title: 'Flutter Demo', home: Home());
   }
 }
---
+++ lib/screens/tasks/home.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:process_flow/provider/task_provider.dart';
+import 'package:provider/provider.dart';
+
+import 'settingsForm.dart';
+import 'taskList.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    void _showSettingsPanel() {
+      showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return Container(
+              padding: const EdgeInsets.all(10),
+              child: const SettingsForm(),
+            );
+          });
+    }
+
+    return ChangeNotifierProvider(
+      create: (context) => taskProvider(),
+      child: Scaffold(
+        body: Container(
+          color: Colors.amber,
+          child: const TaskList(),
+        ),
+        bottomNavigationBar: BottomAppBar(
+            shape: const CircularNotchedRectangle(),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: const [
+                IconButton(
+                  icon: Icon(Icons.home),
+                  onPressed: null,
+                ),
+                IconButton(
+                  icon: Icon(Icons.school),
+                  onPressed: null,
+                ),
+              ],
+            )),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: _showSettingsPanel,
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      ),
+    );
+  }
+}
