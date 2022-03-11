7d978b531abf5c7c5dd64465f7bbe54f37e49d49
Nedim <mr.ramicevic@gmail.com>
2022-03-11 17:37:10 +0300

    edit-delete

--- lib/provider/task_provider.dart
+++ lib/provider/task_provider.dart
@@ -3,12 +3,19 @@ import 'package:process_flow/models/task.dart';
 
 class TaskProvider extends ChangeNotifier {
   List<TaskModel> taskList = <TaskModel>[
-    TaskModel(taskName: "Nedim", taskDesc: "Rastum"),
-    TaskModel(taskName: "Nedim", taskDesc: "Rastum")
+    TaskModel(taskName: "Nedim", taskDesc: "Rastum", isDone: true, id: 1),
+    TaskModel(taskName: "Nedim", taskDesc: "Rastum", isDone: false, id: 2)
   ];
 
   void addTask(TaskModel task) {
     taskList.add(task);
     notifyListeners();
   }
+
+  void deleteTask(int taskId) {
+    taskList.removeWhere((element) => element.id == taskId);
+    notifyListeners();
+  }
+
+  void edidTask() {}
 }
