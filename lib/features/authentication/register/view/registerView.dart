d768f64c2477ca8e9fec8bc2351492250f1e0abe
Nedim <mr.ramicevic@gmail.com>
2022-06-16 10:42:17 +0300

    registerFolder

---
+++ lib/features/authentication/register/view/registerView.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/material.dart';
+import '../../../../shared/constants.dart';
+import '../../../../shared/loading.dart';
+import '../service/registerService.dart';
+
+class Register extends StatefulWidget {
+  const Register({Key? key, required this.setifSignIn}) : super(key: key);
+  final Function setifSignIn;
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  final AuthService _auth = AuthService();
+  final GlobalKey<FormState> _registerKey = GlobalKey<FormState>();
+  String name = "", lastname = "", email = "", password = "", error = "";
+  bool loading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return loading
+        ? const Loading()
+        : Scaffold(
+            appBar: RegisterAppBar(),
+            body: Container(
+              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+              color: Colors.amberAccent[400],
+              child: Form(
+                  key: _registerKey,
+                  child: Column(
+                    children: <Widget>[
+                      const CustomSizedWidget(
+                        size: 20,
+                      ),
+                      TextFormField(
+                        decoration: textInputDecoration.copyWith(
+                          hintText: "Name",
+                        ),
+                        validator: valideteName,
+                        onChanged: onNameChanged,
+                      ),
+                      const CustomSizedWidget(
+                        size: 20,
+                      ),
+                      TextFormField(
+                        decoration: textInputDecoration.copyWith(
+                          hintText: "Last Name",
+                        ),
+                        validator: valideteName,
+                        onChanged: onLastNameChanged,
+                      ),
+                      const CustomSizedWidget(
+                        size: 20,
+                      ),
+                      TextFormField(
+                        decoration: textInputDecoration.copyWith(
+                          hintText: "E-mail",
+                        ),
+                        validator: validateEmail,
+                        onChanged: onEmailChange,
+                      ),
+                      const CustomSizedWidget(
+                        size: 20,
+                      ),
+                      TextFormField(
+                        decoration: textInputDecoration.copyWith(
+                          hintText: "Password",
+                        ),
+                        validator: validatePassword,
+                        obscureText: true,
+                        onChanged: onPasswordChange,
+                      ),
+                      const CustomSizedWidget(
+                        size: 10,
+                      ),
+                      Text(
+                        error,
+                        style: const TextStyle(color: Colors.red),
+                      ),
+                      const CustomSizedWidget(
+                        size: 10,
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(primary: Colors.purple),
+                        child: const Text("Register"),
+                        onPressed: onRegisterButtonPressed,
+                      )
+                    ],
+                  )),
+            ),
+          );
+  }
+
+  void onRegisterButtonPressed() async {
+    if (_registerKey.currentState!.validate()) {
+      setState(() {
+        loading = true;
+      });
+      dynamic result =
+          await _auth.createUserWithEmailAndPassword(email, password);
+      if (result == null) {
+        setState(() {
+          loading = false;
+          error = "enter a valid email";
+        });
+      }
+    }
+  }
+
+  void onPasswordChange(val) {
+    setState(() {
+      password = val;
+    });
+  }
+
+  void onNameChanged(val) {
+    setState(() {
+      name = val;
+    });
+  }
+
+  void onLastNameChanged(val) {
+    setState(() {
+      lastname = val;
+    });
+  }
+
+  String? validatePassword(value) =>
+      value!.length < 6 ? "at least 6 characters" : null;
+
+  void onEmailChange(val) {
+    setState(() {
+      email = val;
+    });
+  }
+
+  String? validateEmail(value) => value!.isEmpty ? "write an email" : null;
+  String? valideteName(value) =>
+      value!.isEmpty ? "this section cannot be empty" : null;
+
+  AppBar RegisterAppBar() {
+    return AppBar(
+      actions: [
+        ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                primary: Colors.redAccent,
+                padding: const EdgeInsets.symmetric(horizontal: 20)),
+            onPressed: () {
+              widget.setifSignIn();
+            },
+            child: const Text("Sign In"))
+      ],
+      elevation: 0,
+      title: const Text("Register"),
+      backgroundColor: Colors.amberAccent,
+    );
+  }
+}
+
+class CustomSizedWidget extends StatelessWidget {
+  const CustomSizedWidget({
+    Key? key,
+    required this.size,
+  }) : super(key: key);
+  final double size;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: size,
+    );
+  }
+}
